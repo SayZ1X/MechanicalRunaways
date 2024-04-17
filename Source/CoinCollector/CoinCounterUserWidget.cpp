03b2ce8159c2bca3b653e8bd9a986cb3054d671f
SayZ1X <m3m0ryjunior@gmail.com>
2024-04-17 18:08:36 +0300

    Added timer to UI

--- Source/CoinCollector/CoinCounterUserWidget.cpp
+++ Source/CoinCollector/CoinCounterUserWidget.cpp
@@ -6,14 +6,43 @@
 void UCoinCounterUserWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
+
+	ElapsedTime = 0.0f;
+	UpdateInterval = 1.0f;
+
 	ABasePlayer* own_player = Cast<ABasePlayer>(GetOwningPlayerPawn() );
 	if (own_player)
 		own_player->Counter_Collected_Coin_Changed.AddDynamic(this, &ThisClass::Collected_Coin_Changed);  // Св-язываем делегат нашего персонажа с функцией для обновления UI текста
 }
 //------------------------------------------------------------------------------------------------------------
-void UCoinCounterUserWidget::Collected_Coin_Changed(const int New_Count_Collected_Coin)
+void UCoinCounterUserWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+   Super::NativeTick(MyGeometry, InDeltaTime);
+
+   ElapsedTime += InDeltaTime;
+   int minutes = 0;
+   int seconds = 0;
+
+   if (ElapsedTime >= UpdateInterval)
+   {  // Проверяем, прошло ли достаточно времени для обновления текста
+
+      minutes = FMath::FloorToInt(ElapsedTime / 60);
+      seconds = FMath::FloorToInt(FMath::Fmod(ElapsedTime, 60));
+
+      // Обновляем оставшееся время в текстовом блоке
+      if (Timer_Text)
+      {
+         FText TimerText = FText::Format(FText::FromString("{0}:{1}"), FText::AsNumber(minutes), FText::AsNumber(seconds));
+         Timer_Text->SetText(TimerText);
+      }
+
+      ++UpdateInterval; // Обновляем время когда должен обновиться таймер
+   }
+}
+//------------------------------------------------------------------------------------------------------------
+void UCoinCounterUserWidget::Collected_Coin_Changed(const int new_count_collected_coin)
 {
 	check(Collected_Coin_Text_Block);
-	Collected_Coin_Text_Block->SetText(FText::AsNumber(New_Count_Collected_Coin));  // Устанавливаем значения текста в UI в новое значение
+	Collected_Coin_Text_Block->SetText(FText::AsNumber(new_count_collected_coin));  // Устанавливаем значения текста в UI в новое значение
 }
 //------------------------------------------------------------------------------------------------------------
--- Source/CoinCollector/CoinCounterUserWidget.h
+++ Source/CoinCollector/CoinCounterUserWidget.h
@@ -13,11 +13,16 @@ class COINCOLLECTOR_API UCoinCounterUserWidget : public UUserWidget
 
 public:
 	void NativeConstruct();
+	void NativeTick(const FGeometry& MyGeometry, float InDeltaTime);
 
 protected:
-	UPROPERTY(meta = (BindWidget) ) UTextBlock* Collected_Coin_Text_Block; // Обязываем создать текстовый виджет в блюпринте
+	UPROPERTY(meta = (BindWidget) ) UTextBlock* Collected_Coin_Text_Block; // Обязываем создать текстовый виджет для собраных монет в блюпринте
+	UPROPERTY(meta = (BindWidget) ) UTextBlock* Timer_Text; // Обязываем создать текстовый виджет для таймера в блюпринте
 
 private:
-	UFUNCTION() void Collected_Coin_Changed(const int New_Count_Collected_Coin);
+	float ElapsedTime; // Переменная для таймера
+	float UpdateInterval; // Переменная для определения с каким интервалом должен обновляться таймер
+
+	UFUNCTION() void Collected_Coin_Changed(const int new_count_collected_coin);
 };
 //------------------------------------------------------------------------------------------------------------
